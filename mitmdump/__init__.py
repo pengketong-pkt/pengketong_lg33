a0f3462e1dcebf0c13616700c19c15a8f4f7f10a
pengketong <13680586193@139.com>
2020-12-15 08:58:01 +0800

    "mitmproxy_mock"

---
+++ mitmdump/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/15 8:42
+# @Author  : pengketong
---
+++ weixinpo/get_phone.py
@@ -0,0 +1,19 @@
+# import random
+#
+#
+# class GetPhone:
+#
+#
+#     def get_phone(self):
+#         """生成手机号"""
+#
+#         # 最后八位数字
+#         suffix = random.randint(10000000, 99999999)
+#
+#         # 拼接手机号
+#         phone = (f"136{suffix}")
+#         # print(phone)
+#         return phone
+import random
+
+print(random.randint(0,9))
